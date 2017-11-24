7d49faec752a2b50275ce83ef097d7bd047283dd
AbrilAbitia <yoru@outlook.com>
2017-11-24 13:37:04 -0600

    Cambios en templates, se empieza a agregar diseño de login.

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var favicon = require('serve-favicon');
+var session = require('express-session');
+var passport = require('passport');
 
 var db = require('./src/database-connection');
 var alumnos = require('./src/routes/alumnos');
@@ -16,12 +18,53 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(session({secret: 'supernova', saveUninitialized: true, resave: true}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(function (request, response, next) {
+    var error = request.session.error;
+    var message = request.session.notice;
+    var success = request.session.success;
+    delete request.session.error;
+    delete request.session.success;
+    delete request.session.notice;
+    if (error)
+        response.locals.error = error;
+    if (message)
+        response.locals.notice = message;
+    if (success)
+        response.locals.success = success;
+    next();
+});
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 app.get('/', function (request, response) {
-    response.render('home', {title: 'Home', message: "HOME"});
+    response.render('home', {title: 'Home', message: "HOME", user: request.user});
+});
+
+app.get('/signin', function (request, response) {
+    response.render('login');
+});
+
+app.post('/local-reg', passport.authenticate('local-signup', {
+    successRedirect: '/',
+    failureRedirect: '/signin'
+}));
+
+app.post('/login', passport.authenticate('local-signin', {
+    successRedirect: '/',
+    failureRedirect: '/signin'
+}));
+
+app.get('/logout', function (request, response) {
+    var name = request.user.username;
+    console.log("LOGGIN OUT " + request.user.username)
+    request.logout();
+    response.redirect('/');
+    request.session.notice = "You have successfully been logged out " + name + "!";
 });
 
 app.use('/alumnos', alumnos);
